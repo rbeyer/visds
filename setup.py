79568643d061083cefe7138fcaafee50c64b9cd5
Other Orb / Moses Milazzo <moses@otherorb.net>
2022-11-30 12:10:40 -0700

    renamed visds to visdag

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ import setuptools
 
 setuptools.setup(
     name="visds",
-    packages=setuptools.find_packages(exclude=["visds_tests"]),
+    packages=setuptools.find_packages(exclude=["visdag_tests"]),
     install_requires=[
         "dagster==0.15.7",
         "dagit==0.15.7",
